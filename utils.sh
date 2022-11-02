8c8276e00315dc627c1dab720fd2068d0c85e0c1
Michał Łyszczek <michal.lyszczek@bofc.pl>
2022-11-02 11:00:32 +0100

    utils.c: add date related functions
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- utils.sh
+++ utils.sh
@@ -40,6 +40,15 @@ print_help()
 	cat $help_file
 }
 
+
+## ==========================================================================
+#                            ____/ /____ _ / /_ ___
+#                           / __  // __ `// __// _ \
+#                          / /_/ // /_/ // /_ /  __/
+#                          \__,_/ \__,_/ \__/ \___/
+## ==========================================================================
+
+
 displaytime()
 {
 	local T=$1
@@ -51,6 +60,76 @@ displaytime()
 	(( D > 0 || H > 0 )) && printf '%02d:' $H
 	(( D > 0 || H > 0 || M > 0 )) && printf '%02d:' $M
 	printf '%02d\n' $S
+}
+
+## ==========================================================================
+#   Returns day number of first working day for the mon-year
+#       mon    month (1-12)
+#       year   year on format YYYY
+## ==========================================================================
+date_get_first_working_day()
+{
+	LC_ALL=C cal $1 $2 | awk 'NR<3{next} {
+	if(NF==1){
+		next;print $1;exit
+	}
+	if(NF<7){
+		print $1;exit
+	}
+	print $2;exit
+}'
+}
+
+## ==========================================================================
+#   Returns day number of first day for the mon-year
+#       day    day to get (su mo tu we th fr sa)
+#       mon    month (1-12)
+#       year   year on format YYYY
+#
+#   example:
+#       $0 sa 5 2021    will return day of a first saturday of may 2021
+## ==========================================================================
+date_get_first_day_of_month()
+{
+	case $1 in
+		su) d=1 ;;
+		mo) d=2 ;;
+		tu) d=3 ;;
+		we) d=4 ;;
+		th) d=5 ;;
+		fr) d=6 ;;
+		sa) d=7 ;;
+		*) echo unkown day; exit 1 ;;
+	esac
+
+	dates=$(LC_ALL=C cal $2 $3 |
+		# skip first (textual date like February 2022)
+		# and second (su mo tu we th fr sa) lines
+		tail -n +3 |
+		# we only need two rows of dates, kill the spare
+		head -n 2 |
+		# replace empty column in first week with '-'
+		sed 's/   / - /g' |
+		# get column coresponding to specified <day>
+		awk "{print \$$d}")
+
+	date=$(echo "$dates" | head -n 1)
+	if [ x$date = x- ]; then
+		# empty date, real date is in second row
+		date=$(echo "$dates" | tail -n 1)
+	fi
+
+	echo $date
+}
+
+## ==========================================================================
+#   Returns last day of the month
+#       mon    month (1-12)
+#       year   year on format YYYY
+## ==========================================================================
+date_get_last_day_of_month()
+{
+	LC_ALL=C cal $1 $2 | tr '\n' ' ' | rev | awk '{print $1}' | rev
 }
 
 # sends stdin to phone
