7bbeade657b9fb45683433fa25cf513c25594d55
Michał Łyszczek <michal.lyszczek@bofc.pl>
2024-02-05 19:51:45 +0100

    add script to toggle between monitor inputs
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

---
+++ toggle-monitor-input-source.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+. /usr/bofc/lib/utils.sh
+cat << HELP | help ne 0 $@
+Toggles between hdmi-1 and hdmi-2 source on my dell screen, should work
+on any other screen as well, but values for source may be different
+HELP
+
+monitor_index=1
+
+current=$(ddcutil --brief -d $monitor_index getvcp 60 | awk '{print $4}')
+if ! test $current; then
+	echo ddcutil --brief -d $monitor_index getvcp 60 returned empty value
+	exit 1
+fi
+
+next=0x11
+if [ $current = x11 ]; then
+	next=0x12
+fi
+
+ddcutil -d 1 setvcp 60 $next
